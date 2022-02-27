3a4f21c5861e51112b7f620f311e90bf0c7b0127
daiki0381 <daiki03810218@gmail.com>
2022-02-27 22:51:11 +0900

    Fix bowling.rb

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,73 +1,41 @@
-# コマンドライン引数の取得
+# frozen_string_literal: true
+
 score = ARGV[0]
-# 1投毎に分割
-scores = score.split(",")
-# 数字に変換
-shots = []
-scores.each {|s| s == "X" ? shots << "X" : shots << s.to_i}
-# 最大のフレーム数
-max_frame = 10
-# フレーム
+scores = score.split(',')
+shots = scores.map { |s| s == 'X' ? 'X' : s.to_i }
+
 frames = []
-max_frame.times {frames.push([])}
-# フレームのインデックス番号
+10.times { frames << [] }
+
 frames_index = 0
-# shotsのインデックス番号
-shots_index = 0
-# 投球数
-count = 0
-# フレーム毎に分割
 shots.each do |shot|
-  # 最終フレームの場合
   if frames_index == 9
-    shot == "X" ? frames[frames_index] << 10 : frames[frames_index] << shot
-  # 最終フレームではない場合
+    frames[frames_index] << (shot == 'X' ? 10 : shot)
+  elsif shot == 'X'
+    frames[frames_index] << 10
+    frames_index += 1
   else
-    # ストライクを取った場合
-    if shot == "X"
-      frames[frames_index] << 10
-      frames_index += 1
-    # 2投した場合
-    else
-      count += 1
-      if count % 2 == 0
-        frames[frames_index] << shots[shots_index - 1] << shot
-        frames_index += 1
-      end
-    end
+    frames[frames_index] << shot
+    frames_index += 1 if frames[frames_index].length == 2
   end
-  # shotsのインデックス番号+1
-  shots_index += 1
 end
-# 合計
+
 total = 0
-# 合計を算出
 frames.each_with_index do |frame, index|
-  # 最終フレームの場合
-  if index == 9
-    total += frame.sum
-  # 最終フレームではない場合
-  else
-    # ストライクの場合
-    if frame[0] == 10
-      # 次がストライクではない場合
-      if frames[index+1][0] != 10
-        total += frame[0] + frames[index+1][0] + frames[index+1][1]
-      # 次もストライク&次が最終フレーム
-      elsif frames[index+1][0] == 10 && index + 1 == 9
-        total += frame[0] + frames[index+1][0] + frames[index+1][1]
-      # 次もストライク&次が最終フレームではない場合
-      elsif frames[index+1][0] == 10 && index + 1 != 9
-        total += frame[0] + frames[index+1][0] + frames[index+2][0]
+  total +=
+    if index == 9
+      frame.sum
+    elsif frame[0] == 10
+      if frames[index + 1][0] == 10 && index + 1 != 9
+        frame[0] + frames[index + 1][0] + frames[index + 2][0]
+      else
+        frame[0] + frames[index + 1][0] + frames[index + 1][1]
       end
-    # スペアの場合
     elsif frame.sum == 10
-      total += frame.sum + frames[index+1][0]
-    # ストライクでもスペアでもない場合
+      frame.sum + frames[index + 1][0]
     else
-      total += frame.sum
+      frame.sum
     end
-  end
 end
-# 合計を出力
+
 puts total
