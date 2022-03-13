0188d671e6fa55bdb4645badbc3d650e4c3d8436
daiki0381 <daiki03810218@gmail.com>
2022-03-13 14:49:02 +0900

    Fix ls2.rb

--- 05.ls/ls2.rb
+++ 05.ls/ls2.rb
@@ -5,21 +5,34 @@ require 'optparse'
 
 NUMBER_OF_COLUMNS = 3
 
-option = ARGV.getopts('a')
-dirs = option['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
-arrs_containing_dirs = Array.new(NUMBER_OF_COLUMNS) { [] }
-number_of_elements_per_column = Rational(dirs.size, NUMBER_OF_COLUMNS).ceil
-index = 0
-dirs.each do |dir|
-  arrs_containing_dirs[index] << dir
-  index += 1 if (arrs_containing_dirs[index].size % number_of_elements_per_column).zero?
+def option
+  @option ||= ARGV.getopts('a')
 end
-unity_number_of_arr_elements = arrs_containing_dirs.map { |arr| arr.values_at(0...number_of_elements_per_column) }
-arrs_with_rows_and_columns_swapped = unity_number_of_arr_elements.transpose
-maximum_number_of_words = dirs.map(&:size).max
-arrs_with_rows_and_columns_swapped.each do |arr|
-  arr.each do |dir|
-    print dir.to_s.ljust(maximum_number_of_words + 7)
+
+def dirs
+  option['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
+end
+
+def add_dirs_to_arrs
+  arrs_containing_dirs = Array.new(NUMBER_OF_COLUMNS) { [] }
+  number_of_elements_per_column = Rational(dirs.size, NUMBER_OF_COLUMNS).ceil
+  index = 0
+  dirs.each do |dir|
+    arrs_containing_dirs[index] << dir
+    index += 1 if (arrs_containing_dirs[index].size % number_of_elements_per_column).zero?
   end
-  print "\n"
+  unity_number_of_arr_elements = arrs_containing_dirs.map { |arr| arr.values_at(0...number_of_elements_per_column) }
+  unity_number_of_arr_elements.transpose
 end
+
+def output_dirs
+  maximum_number_of_words = dirs.map(&:size).max
+  add_dirs_to_arrs.each do |arr|
+    arr.each do |dir|
+      print dir.to_s.ljust(maximum_number_of_words + 7)
+    end
+    print "\n"
+  end
+end
+
+output_dirs
