cbb6ed577ae19850fe12737760db4aa3e54c455b
daiki0381 <daiki03810218@gmail.com>
2022-03-27 18:51:16 +0900

    dirsメソッドをローカル変数に入れる

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -31,11 +31,9 @@ def option
   ARGV.getopts('l')
 end
 
-def dirs
-  Dir.glob('*').sort
-end
+dirs = Dir.glob('*').sort
 
-def file_types_and_file_modes
+def file_types_and_file_modes(dirs)
   dirs.map do |dir|
     file_type = File.stat(dir).ftype.gsub(/fifo|characterSpecial|directory|blockSpecial|file|link|socket/, FILE_TYPE)
     file_mode = File.stat(dir).mode.to_s(8).slice(-3, 3).gsub(/[01234567]/, PERMISSION_PATTERN)
@@ -43,61 +41,61 @@ def file_types_and_file_modes
   end
 end
 
-def number_of_hard_links
+def number_of_hard_links(dirs)
   dirs.map { |dir| File.stat(dir).nlink }
 end
 
-def owners
+def owners(dirs)
   dirs.map { |dir| Etc.getpwuid(File.stat(dir).uid).name }
 end
 
-def groups
+def groups(dirs)
   dirs.map { |dir| Etc.getgrgid(File.stat(dir).gid).name }
 end
 
-def file_sizes
+def file_sizes(dirs)
   dirs.map { |dir| File.stat(dir).size }
 end
 
-def final_update_dates
+def final_update_dates(dirs)
   dirs.map { |dir| File.stat(dir).mtime.strftime('%-m %d %H:%M') }
 end
 
-def files
+def files(dirs)
   dirs.map { |dir| File.symlink?(dir) ? "#{dir} -> #{File.readlink(dir)}" : dir }
 end
 
-def add_file_details_to_arrs
+def add_file_details_to_arrs(dirs)
   [
-    file_types_and_file_modes,
-    number_of_hard_links,
-    owners,
-    groups,
-    file_sizes,
-    final_update_dates,
-    files
+    file_types_and_file_modes(dirs),
+    number_of_hard_links(dirs),
+    owners(dirs),
+    groups(dirs),
+    file_sizes(dirs),
+    final_update_dates(dirs),
+    files(dirs)
   ].transpose
 end
 
-def collect_maximum_number_of_characters
+def collect_maximum_number_of_characters(dirs)
   {
-    maximum_number_of_hard_links_in_characters: number_of_hard_links.map { |number_of_hard_link| number_of_hard_link.to_s.size }.max,
-    maximum_number_of_characters_for_owners: owners.map { |owner| owner.to_s.size }.max,
-    maximum_number_of_characters_for_groups: groups.map { |group| group.to_s.size }.max,
-    maximum_file_sizes_in_characters: file_sizes.map { |file_size| file_size.to_s.size }.max,
-    maximum_number_of_characters_for_final_update_dates: final_update_dates.map { |final_update_date| final_update_date.to_s.size }.max,
-    maximum_number_of_characters_in_files: files.map { |file| file.to_s.size }.max
+    maximum_number_of_hard_links_in_characters: number_of_hard_links(dirs).map { |number_of_hard_link| number_of_hard_link.to_s.size }.max,
+    maximum_number_of_characters_for_owners: owners(dirs).map { |owner| owner.to_s.size }.max,
+    maximum_number_of_characters_for_groups: groups(dirs).map { |group| group.to_s.size }.max,
+    maximum_file_sizes_in_characters: file_sizes(dirs).map { |file_size| file_size.to_s.size }.max,
+    maximum_number_of_characters_for_final_update_dates: final_update_dates(dirs).map { |final_update_date| final_update_date.to_s.size }.max,
+    maximum_number_of_characters_in_files: files(dirs).map { |file| file.to_s.size }.max
   }
 end
 
-def output_total_number_of_blocks
+def output_total_number_of_blocks(dirs)
   total_number_of_blocks = dirs.map.sum { |dir| File.stat(dir).blocks }
   puts "total #{total_number_of_blocks}"
 end
 
-def output_file_details(maximum_number_of_characters)
-  output_total_number_of_blocks
-  add_file_details_to_arrs.each do |arr|
+def output_file_details(dirs, maximum_number_of_characters)
+  output_total_number_of_blocks(dirs)
+  add_file_details_to_arrs(dirs).each do |arr|
     file_types_and_file_modes = arr[0]
     number_of_hard_links = arr[1]
     owners = arr[2]
@@ -116,7 +114,7 @@ def output_file_details(maximum_number_of_characters)
   end
 end
 
-def add_dirs_to_arrs
+def add_dirs_to_arrs(dirs)
   arrs_containing_dirs = Array.new(NUMBER_OF_COLUMNS) { [] }
   number_of_elements_per_column = Rational(dirs.size, NUMBER_OF_COLUMNS).ceil
   index = 0
@@ -128,9 +126,9 @@ def add_dirs_to_arrs
   unity_number_of_arr_elements.transpose
 end
 
-def output_dirs
+def output_dirs(dirs)
   maximum_number_of_words = dirs.map(&:size).max
-  add_dirs_to_arrs.each do |arr|
+  add_dirs_to_arrs(dirs).each do |arr|
     arr.each do |dir|
       print dir.to_s.ljust(maximum_number_of_words + 7)
     end
@@ -138,4 +136,4 @@ def output_dirs
   end
 end
 
-option['l'] ? output_file_details(collect_maximum_number_of_characters) : output_dirs
+option['l'] ? output_file_details(dirs, collect_maximum_number_of_characters(dirs)) : output_dirs(dirs)
