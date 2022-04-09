de45a71ed337c21d4972f0c2a5deb3890db614a8
daiki0381 <daiki03810218@gmail.com>
2022-04-09 18:19:04 +0900

    引数が複数の場合

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
-file = ARGV[0]
-file_contents = File.read(file)
-puts "#{file_contents.lines.size.to_s.rjust(8)} #{file_contents.split(/\s+/).size.to_s.rjust(7)} #{File::Stat.new(file).size.to_s.rjust(7)} #{file}"
+# file = ARGV[0]
+# file_contents = File.read(file)
+# puts "#{file_contents.lines.size.to_s.rjust(8)} #{file_contents.split(/\s+/).size.to_s.rjust(7)} #{File::Stat.new(file).size.to_s.rjust(7)} #{file}"
+
+total_of_lines = 0
+total_of_words = 0
+total_of_bytes = 0
+
+ARGV.each do |file|
+  file_contents = File.read(file)
+  puts "#{file_contents.lines.size.to_s.rjust(8)} #{file_contents.split(/\s+/).size.to_s.rjust(7)} #{File::Stat.new(file).size.to_s.rjust(7)} #{file}"
+  total_of_lines += file_contents.lines.size
+  total_of_words += file_contents.split(/\s+/).size
+  total_of_bytes += File::Stat.new(file).size
+end
+puts "#{total_of_lines.to_s.rjust(8)} #{total_of_words.to_s.rjust(7)} #{total_of_bytes.to_s.rjust(7)} total"
