e11e7458c98751eabc80c9638fea57216c543e35
daiki0381 <daiki03810218@gmail.com>
2022-04-15 16:48:05 +0900

    shot.rbの作成

--- 07.bowling_object/bowling_object.rb
+++ 07.bowling_object/bowling_object.rb
@@ -1,17 +1,5 @@
 # frozen_string_literal: true
 
-class Shot
-  # ①コマンドライン引数をインスタンス変数に入れる。
-  def initialize(shots)
-    @shots = shots
-  end
-
-  # ②使える形に処理して、配列にする。
-  def store_shots_to_array
-    @shots.split(',').map { |shot| shot == 'X' ? 'X' : shot.to_i }
-  end
-end
-
 shot_object = Shot.new(ARGV[0])
 shots = shot_object.store_shots_to_array
 
---
+++ 07.bowling_object/shot.rb
@@ -0,0 +1,11 @@
+class Shot
+  # ①コマンドライン引数をインスタンス変数に入れる。
+  def initialize(shots)
+    @shots = shots
+  end
+
+  # ②使える形に処理して、配列にする。
+  def store_shots_to_array
+    @shots.split(',').map { |shot| shot == 'X' ? 'X' : shot.to_i }
+  end
+end
