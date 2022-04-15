cc1d622a3c612ed849f1b675ddb005cdf9dc2537
daiki0381 <daiki03810218@gmail.com>
2022-04-15 16:47:04 +0900

    Game.rbの作成

--- 07.bowling_object/bowling_object.rb
+++ 07.bowling_object/bowling_object.rb
@@ -44,42 +44,5 @@ end
 frame_object = Frame.new(shots)
 frames = frame_object.divide_into_frames
 
-class Game
-  # ①フレームに分割した配列をインスタンス変数に入れる。
-  def initialize(frames)
-    @frames = frames
-  end
-
-  # ②合計点を算出する。
-  def calculate_total_score
-    total = 0
-    @frames.each_with_index do |frame, index|
-      total +=
-        if index == 9
-          frame.sum
-        elsif frame[0] == 10
-          calculate_strike(index, frame)
-        elsif frame.sum == 10
-          calculate_spare(index, frame)
-        else
-          frame.sum
-        end
-    end
-    total
-  end
-
-  def calculate_strike(index, frame)
-    if @frames[index + 1][0] == 10 && index + 1 != 9
-      frame[0] + @frames[index + 1][0] + @frames[index + 2][0]
-    else
-      frame[0] + @frames[index + 1][0] + @frames[index + 1][1]
-    end
-  end
-
-  def calculate_spare(index, frame)
-    frame.sum + @frames[index + 1][0]
-  end
-end
-
 game_object = Game.new(frames)
 p game_object.calculate_total_score
---
+++ 07.bowling_object/game.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Game
+  # ①フレームに分割した配列をインスタンス変数に入れる。
+  def initialize(frames)
+    @frames = frames
+  end
+
+  # ②合計点を算出する。
+  def calculate_total_score
+    total = 0
+    @frames.each_with_index do |frame, index|
+      total +=
+        if index == 9
+          frame.sum
+        elsif frame[0] == 10
+          calculate_strike(index, frame)
+        elsif frame.sum == 10
+          calculate_spare(index, frame)
+        else
+          frame.sum
+        end
+    end
+    total
+  end
+
+  def calculate_strike(index, frame)
+    if @frames[index + 1][0] == 10 && index + 1 != 9
+      frame[0] + @frames[index + 1][0] + @frames[index + 2][0]
+    else
+      frame[0] + @frames[index + 1][0] + @frames[index + 1][1]
+    end
+  end
+
+  def calculate_spare(index, frame)
+    frame.sum + @frames[index + 1][0]
+  end
+end
