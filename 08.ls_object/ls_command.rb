3c37f0f8606929de5ed7e0f07e6a46e0a8b35831
daiki0381 <daiki03810218@gmail.com>
2022-07-17 16:08:18 +0900

    ♻️ Rename variables in the ls command class

--- 08.ls_object/ls_command.rb
+++ 08.ls_object/ls_command.rb
@@ -11,9 +11,10 @@ class LsCommand
   end
 
   def output_ls_command
-    dir = @option['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
-    dir = @option['r'] ? dir.reverse.map { |file| FileInfo.new(file) } : dir.map { |file| FileInfo.new(file) }
-    @option['l'] ? FileInfoList.new(dir).output_file_details_list : FileList.new(dir).output_file_list
+    sorted_files = @option['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
+    reversed_files = @option['r'] ? sorted_files.reverse : sorted_files
+    file_info_data_list = reversed_files.map { |file_name| FileInfo.new(file_name) }
+    @option['l'] ? FileInfoList.new(file_info_data_list).output_file_details_list : FileList.new(file_info_data_list).output_file_list
   end
 end
 
