668fad2f2a14e531ba889c1984a437bd6df33c01
daiki0381 <daiki03810218@gmail.com>
2022-07-24 14:50:44 +0900

    🐎 Improve performance

--- 08.ls_object/long_format.rb
+++ 08.ls_object/long_format.rb
@@ -30,16 +30,13 @@ class LongFormat
 
   def output
     puts "total #{total_block_count}"
-    hard_link_count_max_length = max_length_list[:hard_link_count_max_length]
-    owner_name_max_length = max_length_list[:owner_name_max_length]
-    group_name_max_length = max_length_list[:group_name_max_length]
-    size_max_length = max_length_list[:size_max_length]
+    max_length_list = build_max_length_list
     build_file_data_list.each do |file_data_list|
       print "#{file_data_list[:type_and_permission]}  "
-      print "#{file_data_list[:hard_link_count].to_s.rjust(hard_link_count_max_length)} "
-      print "#{file_data_list[:owner_name].to_s.ljust(owner_name_max_length)}  "
-      print "#{file_data_list[:group_name].to_s.ljust(group_name_max_length)}  "
-      print "#{file_data_list[:size].to_s.rjust(size_max_length)}  "
+      print "#{file_data_list[:hard_link_count].to_s.rjust(max_length_list[:hard_link_count_max_length])} "
+      print "#{file_data_list[:owner_name].to_s.ljust(max_length_list[:owner_name_max_length])}  "
+      print "#{file_data_list[:group_name].to_s.ljust(max_length_list[:group_name_max_length])}  "
+      print "#{file_data_list[:size].to_s.rjust(max_length_list[:size_max_length])}  "
       print "#{file_data_list[:formatted_time].to_s.rjust(FORMATTED_TIME_LENGTH)} "
       print file_data_list[:name_and_symbolic_link].to_s
       print "\n"
@@ -58,21 +55,18 @@ class LongFormat
     file_info.final_update_time.strftime('%-m %d %H:%M')
   end
 
-  def nested_length_list
-    @file_info_list.map do |file_info|
-      [
-        file_info.hard_link_count.to_s.size,
-        file_info.owner_name.size,
-        file_info.group_name.size,
-        file_info.size.to_s.size
-      ]
-    end.transpose
-  end
-
-  def max_length_list
-    name_list = %i[hard_link_count_max_length owner_name_max_length group_name_max_length size_max_length]
-    max_length_list = nested_length_list.map(&:max)
-    name_list.zip(max_length_list).to_h
+  def build_max_length_list
+    max_length_list = {}
+    @file_info_list.each do |file_info|
+      if file_info.hard_link_count.to_s.size > max_length_list[:hard_link_count_max_length].to_i
+        max_length_list[:hard_link_count_max_length] =
+          file_info.hard_link_count.to_s.size
+      end
+      max_length_list[:owner_name_max_length] = file_info.owner_name.size if file_info.owner_name.size > max_length_list[:owner_name_max_length].to_i
+      max_length_list[:group_name_max_length] = file_info.group_name.size if file_info.group_name.size > max_length_list[:group_name_max_length].to_i
+      max_length_list[:size_max_length] = file_info.size.to_s.size if file_info.size.to_s.size > max_length_list[:size_max_length].to_i
+    end
+    max_length_list
   end
 
   def total_block_count
