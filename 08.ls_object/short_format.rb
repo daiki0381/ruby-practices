45ec807241cdd3a2e2e0a8c271294166d1aa4fed
daiki0381 <daiki03810218@gmail.com>
2022-07-26 22:45:18 +0900

    ♻️ Refactor short format class

--- 08.ls_object/short_format.rb
+++ 08.ls_object/short_format.rb
@@ -8,22 +8,38 @@ class ShortFormat
   end
 
   def output
-    nested_file_info_list = Array.new(COL_COUNT) { [] }
-    file_info_count_per_col = Rational(@file_info_list.size, COL_COUNT).ceil
-    index = 0
-    @file_info_list.each do |file_info|
-      nested_file_info_list[index] << file_info.name_and_symbolic_link
-      index += 1 if (nested_file_info_list[index].size % file_info_count_per_col).zero?
-    end
-    transposed_nested_file_info_list = nested_file_info_list.map do |file_info_list|
-      file_info_list.values_at(0...file_info_count_per_col)
-    end.transpose
-    file_name_max_length = @file_info_list.map { |file_info| file_info.name_and_symbolic_link.size }.max
-    transposed_nested_file_info_list.each do |file_info_list|
+    build_nested_file_info_list
+    transposed_file_info_list.each do |file_info_list|
       file_info_list.each do |file_info|
-        print file_info.to_s.ljust(file_name_max_length + 7)
+        print file_info.file_name.ljust(file_name_max_length + 7)
       end
       puts
     end
+  end
+
+  private
+
+  def file_info_count_per_col
+    Rational(@file_info_list.size, COL_COUNT).ceil
+  end
+
+  def file_name_max_length
+    @file_info_list.map { |file_info| file_info.file_name.size }.max
+  end
+
+  def build_nested_file_info_list
+    nested_file_info_list = Array.new(COL_COUNT) { [] }
+    index = 0
+    @file_info_list.each do |file_info|
+      nested_file_info_list[index] << file_info
+      index += 1 if (nested_file_info_list[index].size % file_info_count_per_col).zero?
+    end
+    nested_file_info_list
+  end
+
+  def transposed_file_info_list
+    build_nested_file_info_list.map do |file_info_list|
+      file_info_list.values_at(0...file_info_count_per_col)
+    end.transpose.map(&:compact)
   end
 end
